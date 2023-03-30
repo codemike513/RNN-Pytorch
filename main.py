a96c0b52ffe3fbcaf30f969bf81b3ddb0a52b99a
Mihir Pesswani <72348929+codemike513@users.noreply.github.com>
2023-03-30 09:21:25 +0000

    Prediction using RNN

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 from utils import ALL_LETTERS, N_LETTERS
 from utils import load_data, letter_to_tensor, line_to_tensor, random_training_example
@@ -61,4 +61,65 @@ print(category_from_output(output))
 
 criterion = nn.NLLLoss()
 learning_rate = 0.005
-optimizer = torch.optim.SGD(rnn.paramters(), lr=learning_rate)
+optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate)
+
+
+def train(line_tensor, category_tensor):
+  hidden = rnn.init_hidden()
+
+  for i in range(line_tensor.size()[0]):
+    output, hidden = rnn(line_tensor[i], hidden)
+
+  loss = criterion(output, category_tensor)
+
+  optimizer.zero_grad()
+  loss.backward()
+  optimizer.step()
+
+  return output, loss.item()
+
+
+current_loss = 0
+all_losses = []
+plot_steps, print_steps = 1000, 100
+n_iters = 100000
+for i in range(n_iters):
+  category, line, category_tensor, line_tensor = random_training_example(
+    category_lines, all_categories)
+
+  output, loss = train(line_tensor, category_tensor)
+  current_loss += loss
+
+  if (i + 1) % plot_steps == 0:
+    all_losses.append(current_loss / plot_steps)
+    current_loss = 0
+
+  if (i + 1) % print_steps == 0:
+    guess = category_from_output(output)
+    correct = "CORRECT" if guess == category else f"WRONG ({category})"
+    print(f"{i} {i/n_iters*100} {loss:.4f} {line} / {guess} {correct}")
+
+plt.figure()
+plt.plot(all_losses)
+plt.show()
+
+
+def predict(input_line):
+  print(f"\n> {input_line}")
+  with torch.no_grad():
+    line_tensor = line_to_tensor(input_line)
+    hidden = rnn.init_hidden()
+
+    for i in range(line_tensor.size()[0]):
+      output, hidden = rnn(line_tensor[i], hidden)
+
+    guess = category_from_output(output)
+    print(guess)
+
+
+while True:
+  sentence = input("Input: ")
+  if sentence == "quit":
+    break
+
+  predict(sentence)
